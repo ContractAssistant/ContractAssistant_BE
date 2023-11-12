95c3dcc2aa4186628f60c991fdedad1837a14b68
sami355-24 <ddung355@gmail.com>
2023-11-12 18:13:44 +0900

    [feat]: 스프링 시작점 추가

---
+++ src/test/java/MtPalgong/legalAssistant/LegalAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package MtPalgong.legalAssistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegalAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
